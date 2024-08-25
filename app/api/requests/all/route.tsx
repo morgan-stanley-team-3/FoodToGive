91069172af779014f113d6c4fdda49709d75e577
Shinkeishi <ashleyytanjx@gmail.com>
2024-08-26 03:18:37 +0800

    Added get all routes for requests and donations

---
+++ app/api/requests/all/route.tsx
@@ -0,0 +1,40 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+declare global {
+    var _mongoClientPromise: Promise<MongoClient>;
+}
+
+const uri = process.env.MONGODB_URI as string;
+if (!uri) {
+    throw new Error("Please add your MongoDB URI to .env.local");
+}
+
+let mongoClient: MongoClient = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function getCollection() {
+    const client = await mongoClient.connect();
+    const db = client.db("database");
+    return db.collection("requests");
+}
+
+export async function GET(request: NextRequest) {
+    try {
+        const collection = await getCollection();
+        // find donations made by donor with user.email
+        const requests = await collection.find({}).toArray();
+        return NextResponse.json(requests, { status: 200 });
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 404 }
+        );
+    }
+}
