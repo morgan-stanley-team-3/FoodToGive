a501347c5113b4d27a6359f7bb09c759077f5487
Shinkeishi <ashleyytanjx@gmail.com>
2024-08-25 19:22:07 +0800

    Updated logic for cooked and non-cooked food and added icons

--- app/beneficiaryDashboard/BeneficiaryCard.tsx
+++ app/beneficiaryDashboard/BeneficiaryCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     Card,
     CardBody,
@@ -13,6 +15,12 @@ import {
 } from "react-icons/fa";
 import { FaBowlFood } from "react-icons/fa6";
 import { Request } from "./BeneficiaryDashboardClient";
+import { useState, useEffect } from "react";
+import { AiOutlineNumber } from "react-icons/ai";
+import { FaRegStar } from "react-icons/fa6";
+import { FaTruck } from "react-icons/fa";
+import { FaPersonChalkboard } from "react-icons/fa6";
+import { IoLocation } from "react-icons/io5";
 
 interface RequestCardProps {
     request: Request;
@@ -20,44 +28,92 @@ interface RequestCardProps {
 
 const BeneficiaryCard: React.FC<RequestCardProps> = ({ request }) => {
     console.log(request);
+    const [isCooked, setIsCooked] = useState(false);
+    const [isSelfCollection, setIsSelfCollection] = useState(false);
+    const [isDelivery, setIsDelivery] = useState(false);
+
+    useEffect(() => {
+        if (request.foodType === "Cooked Food") {
+            setIsCooked(true);
+        }
+
+        if (request.deliveryMethod === "Self-Collection") {
+            setIsSelfCollection(true);
+        } else {
+            setIsDelivery(true);
+        }
+    }, [request]);
+
     return (
         <Card
             shadow={false}
             className="relative mb-4 border border-black rounded-lg p-4"
         >
             <CardBody>
-                <Typography variant="h5" color="blue-gray" className="mb-2">
-                    <FaBowlFood className="inline-block mr-2" />
-                    {request.foodName} [{request.foodCategory}]
-                </Typography>
-                <Typography className="mb-2">
-                    <FaQuestionCircle className="inline-block mr-2" />
-                    Quantity: {request.quantity}
-                </Typography>
+                {!isCooked && (
+                    <Typography variant="h5" color="blue-gray" className="mb-2">
+                        <FaBowlFood className="inline-block mr-2" />
+                        {request.foodName} [{request.foodCategory}]
+                    </Typography>
+                )}
+                {isCooked && (
+                    <Typography variant="h5" color="blue-gray" className="mb-2">
+                        <FaBowlFood className="inline-block mr-2" />
+                        {request.foodName}
+                    </Typography>
+                )}
+                {!isCooked && (
+                    <Typography className="mb-2">
+                        <AiOutlineNumber className="inline-block mr-2" />
+                        Quantity: {request.quantity}
+                    </Typography>
+                )}
+                {isCooked && (
+                    <Typography className="mb-2">
+                        <AiOutlineNumber className="inline-block mr-2" />
+                        Number of servings: {request.numberOfServings}
+                    </Typography>
+                )}
                 <Typography className="mb-2">
                     <FaClock className="inline-block mr-2" />
-                    Time of Preparation: {request.timeOfPreparation}
+                    Need by: {request.needByTime}
                 </Typography>
                 <Typography className="mb-2">
-                    <FaCalendarAlt className="inline-block mr-2" />
-                    Time of Consumption: {request.timeOfConsumption}
+                    <FaRegStar className="inline-block mr-2" />
+                    Special Request: {request.specialRequest}
                 </Typography>
-                <Typography className="mb-2">
-                    <FaUtensils className="inline-block mr-2" />
-                    Ingredients: {request.ingredient}
-                </Typography>
-                <Typography className="mb-2">
-                    <FaQuestionCircle className="inline-block mr-2" />
-                    Perishable: {request.perishable}
-                </Typography>
-                <Typography className="mb-2">
-                    <FaQuestionCircle className="inline-block mr-2" />
-                    request Status: {request.requestStatus}
-                </Typography>
-                <Typography className="mb-2">
-                    <FaQuestionCircle className="inline-block mr-2" />
-                    Delivery Method: {request.deliveryMethod}
-                </Typography>
+                {isDelivery && (
+                    <Typography className="mb-2">
+                        <FaTruck className="inline-block mr-2" />
+                        Delivery Method: {request.deliveryMethod}
+                    </Typography>
+                )}
+                {isSelfCollection && (
+                    <Typography className="mb-2">
+                        <FaPersonChalkboard className="inline-block mr-2" />
+                        Delivery Method: {request.deliveryMethod}
+                    </Typography>
+                )}
+                {isSelfCollection && (
+                    <>
+                        <Typography className="mb-2">
+                            <FaClock className="inline-block mr-2" />
+                            Pick-up Time: {request.deliveryTime}
+                        </Typography>
+                    </>
+                )}
+                {isDelivery && (
+                    <>
+                        <Typography className="mb-2">
+                            <FaClock className="inline-block mr-2" />
+                            Delivery Time: {request.deliveryTime}
+                        </Typography>
+                        <Typography className="mb-2">
+                            <IoLocation className="inline-block mr-2" />
+                            Delivery Location: {request.deliveryLocation}
+                        </Typography>
+                    </>
+                )}
             </CardBody>
             <CardFooter className="pt-0">
                 <Button className="text-black">withdraw</Button>
