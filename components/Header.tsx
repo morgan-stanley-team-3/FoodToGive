261e5a0dc86c19f3e66a973db3db4aea2b30e261
George Tay <george_tay@u.nus.edu>
2024-08-25 15:46:11 +0800

    Refine Header component design

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import { signOut, useSession } from 'next-auth/react';
 import React, { useState } from 'react';
 import { Button } from './ui/button';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const Header: React.FC = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
@@ -12,9 +13,20 @@ const Header: React.FC = () => {
 
   return (
     <header className='flex justify-between items-center mb-12 bg-white shadow-md p-4'>
-      <div className='text-3xl font-bold text-green-600'>
-        <a href='/'>FoodToGive - Singapore Food Bank</a>
-      </div>
+      <a href='/'>
+        <div className='flex flex-row items-center'>
+          <span>
+            <Image
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_PW2BsuN_3oL96einYF1zuZdMrTO7MTbXLw&s'
+              alt='Our Mission'
+              width={52}
+              height={52}
+              className='mr-2'
+            />
+          </span>
+          <span className='text-3xl font-bold text-[#A2C765]'>FoodToGive</span>
+        </div>
+      </a>
       <nav className='space-x-6 text-lg'>
         <a href='/' className='text-gray-700 hover:text-[#A2C765]'>
           Home
@@ -51,14 +63,19 @@ const Header: React.FC = () => {
           )}
         </div>
         {!sessionData.data?.user && (
-          <a href='/login' className='text-gray-700 hover:text-[#A2C765]'>
-            Login
+          <a href='/signup' className='text-gray-700 hover:text-[#A2C765]'>
+            Sign Up
           </a>
         )}
         {!sessionData.data?.user && (
-          <a href='/signup' className='text-gray-700 hover:text-[#A2C765]'>
-            Signup
-          </a>
+          <Button
+            onClick={() => {
+              router.push('/login');
+            }}
+            className='bg-[#A2C765] text-white text-md rounded-full hover:bg-[#8BBE3D]'
+          >
+            Login
+          </Button>
         )}
         {sessionData.data?.user && (
           <a href='/account' className='text-gray-700 hover:text-[#A2C765]'>
